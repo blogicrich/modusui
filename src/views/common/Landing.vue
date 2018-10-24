ba0d5ec392da99fd21f15541782e96dbbe6abebf
Richard Reading <richard.reading@ndevr.co.uk>
2018-10-24 23:16:34 +0100

    Login and Landing pages fully responsive. Completed ED-14 and ED-36.

--- src/views/common/Landing.vue
+++ src/views/common/Landing.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="page-container">
-    <div class="landing-sys-admin">
-      <h1 class="pg-header">eDroplet Administration</h1>
-      <v-container v-if="$vuetify.breakpoint.smAndUp" xs12>
-      <!-- <v-container v-else="$vuetify.breakpoint.smAndDown" grid-list-xs6> -->
-        <v-container grid-list-xs,sm,md,lg,xl>
+  <v-container grid-list-md text-xs-center>
+    <h1 class="pg-header">eDroplet Administration</h1>
+      <v-layout row wrap>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Administrator Management</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
+          <v-divider class="mb-2" color="#00a1cd"></v-divider>
           <SubNavButton
             title="System Administrators"
             route="/systemadmins"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Defaults Administration</h2>
           <v-divider class="my-1" color="#00a1cd"></v-divider>
           <SubNavButton
             title="default Intervals"
             route="/defaultintervals"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Data Tables</h2>
           <v-divider class="my-1" color="#00a1cd"></v-divider>
           <SubNavButton
@@ -39,8 +37,8 @@
             title="Conditions"
             route="/conditions"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Message options</h2>
           <v-divider class="my-1" color="#00a1cd"></v-divider>
           <SubNavButton
@@ -51,29 +49,28 @@
             title="Voice Message Defaults"
             route="/voicemessages"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Hydration Parameters</h2>
           <v-divider class="my-1" color="#00a1cd"></v-divider>
           <SubNavButton
             title="Hydration Parameters"
             route="/hydrationparameters"
           />
-        </v-container>
-      </v-container>
-    </div>
+        </v-flex>
+    </v-layout>
     <div class="landing-cli-admin">
       <h1 class="pg-header">Main Menu</h1>
-      <v-container grid-list-xs,sm,md,lg,xl>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        <v-layout row wrap>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Authorised Persons</h2>
           <v-divider class="my-1" color="#00a1cd"></v-divider>
           <SubNavButton
             title="Personal Details"
             route="/personaldetails"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">eDroplet Settings</h2>
           <v-divider color="#00a1cd"></v-divider>
           <SubNavButton
@@ -88,26 +85,26 @@
             title="Conditions"
             route="/conditions"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">eDroplet Management</h2>
           <v-divider color="#00a1cd"></v-divider>
           <SubNavButton
             title="eDroplet Management"
             route="/edropletmanagement"
           />
-        </v-container>
-        <v-container grid-list-xs,sm,md,lg,xl>
+        </v-flex>
+        <v-flex xs12 md6>
           <h2 class="pg-subhead">Carer Settings</h2>
           <v-divider color="#00a1cd"></v-divider>
           <SubNavButton
             title="Personnel Settings"
             route="/personnelsettings"
           />
-        </v-container>
-      </v-container>
+        </v-flex>
+      </v-layout>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -116,18 +113,7 @@ export default {
   name: 'Landing',
   components: {
     SubNavButton
-  },
-  // data () {
-  //   return {
-  //     type: String,
-  //     outline: Boolean,
-  //     btnColor: String,
-  //     btnClass: String,
-  //     block: Boolean,
-  //     title: String,
-  //     route: String
-  //   }
-  // }
+  }
 }
 </script>
 <style scoped lang="scss">
