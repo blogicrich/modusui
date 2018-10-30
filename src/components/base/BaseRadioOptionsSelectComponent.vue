54328e7d08cd926109ab8ac167ee50eb498b8605
Richard Reading <richard.reading@ndevr.co.uk>
2018-10-30 17:03:54 +0000

    Added BaseRadioOptionsSelectComponent.vue. Mocked baseline component features. Work towards ED-29.

---
+++ src/components/base/BaseRadioOptionsSelectComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container fluid column>
+    <v-card class="pa-2">
+      <span class="subheader"> {{ groupHeader }} </span>
+      <v-divider class="mb-2" color="#00a1cd"></v-divider>
+      <v-layout row wrap>
+      </v-layout>
+        <v-layout row fill-height align-start justify-center>
+          <span class="my-2"> {{ groupDescription }} </span>
+          <v-layout class="ma-2" column>
+            <span> {{ radioHeader }}</span>
+            <v-radio-group
+              v-model="value"
+              :mandatory="false"
+              color="primary"
+              row
+            >
+              <v-radio
+                v-for="radio in radioConfig"
+                :key="radio.label"
+                :label="radio.label"
+                :value="radio.value"
+                >
+              </v-radio>
+            </v-radio-group>
+          </v-layout>
+        </v-layout>
+      <!-- </v-layout> -->
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'BaseRadioOptionsSelectComponent',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  props: {
+    radioConfig: Array,
+    groupHeader: String,
+    groupDescription: String,
+    radioHeader: String,
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "./public/scss/main.scss";
+</style>
---
+++ src/views/common/IntervalOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <BaseRadioOptions
+      :radioConfig="radioConfig"
+      :groupHeader="groupHeader"
+      :groupDescription="groupDescription"
+      :radioHeader="radioHeader"
+    />
+  </v-container>
+</template>
+
+<script>
+import BaseRadioOptions from '@/components/base/BaseRadioOptionsSelectComponent.vue'
+export default {
+  name: 'IntervalOptions',
+  components: {
+    BaseRadioOptions
+  },
+  data () {
+    return {
+      radioConfig: [
+        { label: 'Radio-1', value: '1' },
+        { label: 'Radio-2', value: '2' },
+        { label: 'Radio-3', value: '3' },
+        { label: 'Radio-4', value: '4' }
+      ],
+      groupHeader: 'Blue light flashing interval options',
+      groupDescription: 'Time betweeen drink reminders - (Blue light flashing)',
+      radioHeader: 'Please select an option from the following:'
+    }
+  },
+}
+</script>
+<style scoped lang="scss">
+@import "./public/scss/main.scss";
+</style>
