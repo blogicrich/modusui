d4b621b144bf6ae3e0df70cb286d48d5d9a371c7
Tamara <97730@student.drenthecollege.nl>
2018-11-24 14:05:36 +0000

    Work towards Root navigation screen. Work towards ED-40.

---
+++ src/components/base/BaseLinkComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-layout column align-start>
+      <div>
+        <router-link
+          class="text-landing"
+          :to="link"
+          v-bind:style="{color: colorIcon}"
+        >
+          <v-icon
+            :color="colorIcon"
+            :to="link"
+          >
+            {{ icon }}
+          </v-icon>
+          <img alt="" src="test"><img>
+          <span>{{ routerTitel }}</span>
+        </router-link>
+      </div>
+      <div class="text-tooltip">
+        <span>{{ tooltipText }}</span>
+      </div>
+    </v-layout>
+    <br/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BaseLinkComponent',
+  props: {
+    routerTitel: String,
+    link: String,
+    tooltipText: String,
+    icon: String,
+    colorIcon: String,
+    test: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "./public/scss/main.scss";
+</style>
--- src/views/common/Landing.vue
+++ src/views/common/Landing.vue
@@ -3,106 +3,212 @@
     <v-dialog v-if="redirecting" max-width="87%">
     </v-dialog>
     <h1 class="pg-header">eDroplet Administration</h1>
-      <v-layout v-if="this.user == 'SYSTEM ADMINISTRATOR'" row wrap>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary">Administrator Management</h2>
-          <v-divider class="mb-2" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="System Administrators"
-            route="/systemadmins"
-          />
-        </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">Defaults Administration</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Interval Options"
-            route="/intervaloptions"
-          />
-        </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">Data Tables</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Titles"
-            route="/titles"
-          />
-          <SubLandingNavButton
-            title="Container Types"
-            route="/containertypes"
-          />
-          <SubLandingNavButton
-            title="Gender Options"
-            route="/genderoptions"
-          />
-          <SubLandingNavButton
-            title="Conditions Options"
-            route="/conditionsoptions"
-          />
-        </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">Message options</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Text/Email Messages"
-            route="/textmessages"
-          />
-          <SubLandingNavButton
-            title="Voice Message Defaults"
-            route="/voicemessages"
-          />
-        </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">Hydration Parameters</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Hydration Parameters"
-            route="/hydrationparameters"
-          />
-        </v-flex>
+    <v-layout v-if="this.user == 'SYSTEM ADMINISTRATOR'" row wrap>
+      <v-flex xs12 sm10 md6 lg4 offset-lg2 offset-sm1>
+        <v-expansion-panel popout :value="openExp">
+          <v-expansion-panel-content style="background-color:transparent;">
+            <div slot="header">
+              <h2 class="landing-subhead" style="background-color:#003c4d;">
+                <v-icon x-large color="white">table_chart</v-icon>
+                <span>Data Tables</span>
+              </h2>
+            </div>
+            <v-card style="background-color:transparent;">
+              <v-card-text>
+                <BaseLinkComponent
+                  routerTitel= "Titles"
+                  link="/titles"
+                  tooltipText="Define/Edit Titles to be used for Identification"
+                  icon="perm_identity"
+                  colorIcon="#003c4d"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Container Types"
+                  link="/containertypes"
+                  tooltipText="Define/Edit custom container types and their volumes"
+                  icon="local_drink"
+                  colorIcon="#003c4d"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Gender Options"
+                  link="/genderoptions"
+                  tooltipText="Define/Edit Genders to be used for Identification"
+                  icon="wc"
+                  colorIcon="#003c4d"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Conditions Options"
+                  link="/conditionsoptions"
+                  tooltipText="Define/Edit Conditions to be cross-referenced to eDroplet Users"
+                  icon="local_pharmacy"
+                  colorIcon="#003c4d"
+                />
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+      <v-flex xs12 sm10 md6 lg4 offset-sm1 offset-md0>
+        <v-expansion-panel popout :value="openExp">
+          <v-expansion-panel-content style="background-color:transparent;">
+            <div slot="header">
+              <h2 class="landing-subhead" style="background-color:#006480;">
+                <v-icon x-large color="white">notifications_active</v-icon>
+                Message options
+              </h2>
+            </div>
+            <v-card style="background-color:transparent;">
+              <v-card-text>
+                <BaseLinkComponent
+                  routerTitel= "Text/Email Messages"
+                  link="/textmessages"
+                  tooltipText="Set the Alert Text and Email Message templates"
+                  icon="message"
+                  colorIcon="#006480"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Voice Message Defaults"
+                  link="/voicemessages"
+                  tooltipText="Upload and set the default voice messages for new eDroplets"
+                  icon="record_voice_over"
+                  colorIcon="#006480"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Hydration Parameters"
+                  link="/hydrationparameters"
+                  tooltipText="Define the Hydration status boundaries"
+                  icon="local_drink"
+                  colorIcon="#006480"
+                />
+                <BaseLinkComponent
+                  routerTitel= "Interval Options"
+                  link="/intervaloptions"
+                  tooltipText="Define and edit Genders to be used for Identification"
+                  icon="notification_important"
+                  colorIcon="#006480"
+                />
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+      <v-flex xs12 sm10 md6 lg4 offset-lg2 offset-sm1>
+        <v-expansion-panel popout :value="openExp">
+          <v-expansion-panel-content style="background-color:transparent;">
+            <div slot="header">
+              <h2 class="landing-subhead" style="background-color:#00a1cd;">
+                <v-icon x-large color="white">people</v-icon>
+                Admin Management
+              </h2>
+            </div>
+            <v-card style="background-color:transparent;">
+              <v-card-text>
+                <BaseLinkComponent
+                  routerTitel= "System Administrators"
+                  link="/systemadmins"
+                  tooltipText="Add and Manage Authorised System Administrators"
+                  icon="person"
+                  colorIcon="#00a1cd"
+                />
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
     </v-layout>
+
     <div v-if="this.user == 'CLIENT ADMINISTRATOR'" class="landing-cli-admin">
       <h1 class="pg-header">Main Menu</h1>
-        <v-layout row wrap>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">Authorised Persons</h2>
-          <v-divider class="my-1" color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Personal Details"
-            route="/personaldetails"
-          />
+      <v-layout row wrap>
+        <v-flex xs12 sm10 md6 lg4 offset-lg2 offset-sm1>
+          <v-expansion-panel popout :value="openExp">
+            <v-expansion-panel-content style="background-color:transparent;">
+              <div slot="header">
+                <h2 class="landing-subhead" style="background-color:#003c4d;">
+                  <v-icon x-large color="white">people</v-icon>
+                  Authorised Persons
+                </h2>
+              </div>
+              <v-card style="background-color:transparent;">
+                <v-card-text>
+                  <BaseLinkComponent
+                    routerTitel= "Personal Details"
+                    link="/personaldetails"
+                    tooltipText="Add and manage authorised people on this account"
+                    icon="person"
+                    colorIcon="#003c4d"
+                  />
+                  <BaseLinkComponent
+                    routerTitel= "Conditions"
+                    link="/conditions"
+                    tooltipText="Record any conditions and subsequent consumption modifiers for eDroplet Users"
+                    icon="priority_high"
+                    colorIcon="#003c4d"
+                  />
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
         </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">eDroplet Settings</h2>
-          <v-divider color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Voice Message Management"
-            route="/voicemessages"
-          />
-          <SubLandingNavButton
-            title=" Interval Settings Management"
-            route="/intervalsettings"
-          />
-          <SubLandingNavButton
-            title="Conditions"
-            route="/conditions"
-          />
+        <v-flex xs12 sm10 md6 lg4 offset-sm1 offset-md0>
+          <v-expansion-panel popout :value="openExp">
+            <v-expansion-panel-content style="background-color:transparent;">
+              <div slot="header">
+                <h2 class="landing-subhead" style="background-color:#006480;">
+                  <v-icon x-large color="white">local_drink</v-icon>
+                  eDroplet Settings
+                </h2>
+              </div>
+              <v-card style="background-color:transparent;">
+                <v-card-text>
+                  <BaseLinkComponent
+                    routerTitel= "Voice Message Management"
+                    link="/voicemessages"
+                    tooltipText="Upload and set custom voice messages for your eDroplets"
+                    icon="record_voice_over"
+                    colorIcon="#006480"
+                  />
+                  <BaseLinkComponent
+                    routerTitel= "Interval Settings Management"
+                    link="/intervalsettings"
+                    tooltipText="Set the reminder and Communication intervals for your eDroplets"
+                    icon="notification_important"
+                    colorIcon="#006480"
+                  />
+                  <BaseLinkComponent
+                    routerTitel= "eDroplet Management"
+                    link="/edropletmanagement"
+                    tooltipText="Manage the eDroplets on this account. Assign them to users or deactivate them"
+                    icon="local_drink"
+                    colorIcon="#006480"
+                  />
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
         </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary">eDroplet Management</h2>
-          <v-divider color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="eDroplet Management"
-            route="/edropletmanagement"
-          />
-        </v-flex>
-        <v-flex xs12 md6>
-          <h2 class="pg-subheader text-primary text-primary text-center">Carer Settings</h2>
-          <v-divider color="#00a1cd"></v-divider>
-          <SubLandingNavButton
-            title="Personnel Settings"
-            route="/personnelsettings"
-          />
+        <v-flex xs12 sm10 md6 lg4 offset-lg2 offset-sm1>
+          <v-expansion-panel popout :value="openExp">
+            <v-expansion-panel-content style="background-color:transparent;">
+              <div slot="header">
+                <h2 class="landing-subhead" style="background-color:#00a1cd;">
+                  <v-icon x-large color="white">supervisor_account</v-icon>
+                  Carer Settings
+                </h2>
+              </div>
+              <v-card style="background-color:transparent;">
+                <v-card-text>
+                  <BaseLinkComponent
+                    routerTitel= "Personnel Settings"
+                    link="/personnelsettings"
+                    tooltipText="Set the desired Alert notifications for designated Carers"
+                    icon="notifications_active"
+                    colorIcon="#00a1cd"
+                  />
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
         </v-flex>
       </v-layout>
     </div>
@@ -111,13 +217,16 @@
 
 <script>
 import SubLandingNavButton from '@/components/sub/SubLandingNavButton.vue'
+import BaseLinkComponent from '@/components/base/BaseLinkComponent.vue'
 export default {
   name: 'Landing',
   components: {
+    BaseLinkComponent,
     SubLandingNavButton
   },
   data () {
     return {
+      openExp: 1,
       user: JSON.parse(localStorage.auth).level,
       redirecting: false
     }
@@ -127,9 +236,17 @@ export default {
   },
   mounted () {
     console.log(this.$route)
+    if (this.$vuetify.breakpoint.name !== 'xs') {
+      this.openExp = 0
+    }
   }
 }
 </script>
 <style scoped lang="scss">
 @import "./public/scss/main.scss";
+
+img {
+  height: inherit;
+  padding: 5px;
+}
 </style>
