8e01f9b7f026e356285c5f528096de4cbe67c94f
jasper <jasper@ndevr.uk>
2019-02-18 10:44:32 +0000

    created baseChartComponent to display charts, ED-54

---
+++ src/components/sub/subDoughnutChart.vue
@@ -0,0 +1,16 @@
+<script>
+  import { Doughnut, mixins } from 'vue-chartjs';
+  const { reactiveProp } = mixins;
+
+  export default {
+    extends: Doughnut,
+    mixins: [reactiveProp],
+    props: {
+      chartData: Object,
+      options: Object
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
+    }
+  };
+</script>
