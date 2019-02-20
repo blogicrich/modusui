cae95cf91f580351d1d8497bdbc43a3f8702cc9a
Tom Brinkman <tom@ndevr.co.uk>
2019-02-20 11:19:31 +0000

    Tweaked some minor things. Completed ED-52

--- src/views/common/ForgotPassword.vue
+++ src/views/common/ForgotPassword.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container id="forgotpassword">
     <v-flex>
-      <v-layout class="ma-2" column align-center>
-        <img alt src="../../assets/ed_logo.svg">
+      <v-layout class="mt-2 mb-5" column align-center>
+        <router-link to="/login">
+          <img alt src="../../assets/ed_logo.svg">
+        </router-link>
       </v-layout>
       <v-form>
         <v-layout align-center justify-center column fill-height>
-          <h3>Please enter your email address below and click the Reset Password button.</h3>
-          <v-input class="mt-3" id="email" v-model="input.email" label="Enter your Email address"/>
-          <v-btn flat v-on:click="forgotPassword">Reset Password</v-btn>
+          <h3>Please enter your email address below and click the Send E-mail button.</h3>
+          <v-text-field class="mt-3" id="email" v-model="email" type="email" name="email" :rules="emailRules" label="Enter your e-mail address" single-line required/>
+          <v-btn flat v-on:click="forgotPassword">Send E-mail</v-btn>
         </v-layout>
       </v-form>
     </v-flex>
@@ -20,15 +22,36 @@ export default {
   data() {
     return {
       input: {
-        email: null
-      }
+        email: ''
+      },
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid'
+      ],
     };
   },
   methods: {
-    forgotPassword() {
-      // this.$emit(this.input.email);
-      alert('Please check your email for your new password. You can change it after you\'ve logged in using this new password.')
-      console.log(this.input.email);
+    forgotPassword: function (e) {
+      this.errors = [];
+
+      if (!this.email) {
+        alert('Email is required.');
+      }
+      else if (!this.validEmail(this.email)) {
+        alert('E-mail must be valid');
+      }
+      else {
+        alert('Please check your e-mail for a link to reset your password.')
+        this.$router.push("/login")
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
     }
   }
 };
@@ -43,7 +66,7 @@ img {
 h3 {
   margin-top: 5px;
 }
-v-input[id="email"] {
+div.v-input__control {
   width: 40vh;
   padding: 0.5vh;
   margin: 1vh;
@@ -51,7 +74,7 @@ v-input[id="email"] {
   color: $dark-blue-grey;
   border-color: $vuetify-primary;
   border-style: solid;
-  margin-top: 25px;
+  margin-top: -10px;
 }
 button.v-btn {
   color: $dark-blue-grey;
@@ -60,8 +83,8 @@ button.v-btn {
   border-style: solid;
   border-radius: 1vh;
   cursor: pointer;
-  margin-left: 20.5%;
+  margin-left: 257px;
   margin-top: 10px;
-  width: 140px;
+  width: 125px;
 }
 </style>
