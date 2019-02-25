645f8faad147e0359550cb031beb1dc28a17d511
Tom Brinkman <tom@ndevr.co.uk>
2019-02-25 11:58:54 +0000

    Made Calendar and added some examples in there. Work towards ED-51

---
+++ src/views/carer/Away.vue
@@ -0,0 +1,20 @@
+<template>
+  <base-calendar></base-calendar>
+</template>
+
+<script>
+import BaseCalendarComponent from "../../components/base/BaseCalendarComponent.vue";
+
+export default {
+  components: {
+    "base-calendar": BaseCalendarComponent
+  },
+  data: () => ({
+    
+  })
+};
+</script>
+
+<style lang="scss">
+@import "./public/scss/main.scss";
+</style>
