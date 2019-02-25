d98aff4ab15cfa413b87cd8800c17a5a28721eab
Anne van der Meer <anne@ndevr.co.uk>
2019-02-25 10:47:38 +0000

    Created some groundwork for the report component for ED-38

---
+++ src/components/base/BaseReportTableComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout row wrap v-if="table.type === `header`">
+    <v-flex
+      offset-xs1 offset-sm0 xs10 sm6 md4 lg3
+      v-for="(item, index) in table.items"
+      :key="index"
+    >{{item.title}}: {{item.data}}</v-flex>
+  </v-layout>
+  <v-data-table v-else-if="table.type === `table`"></v-data-table>
+</template>
+
+<script>
+export default {
+  name: "BaseReportTable",
+  props: {
+    table: {
+      type: String,
+      items: Array
+    }
+  }
+};
+</script>
--- src/views/carer/Reports.vue
+++ src/views/carer/Reports.vue
@@ -1,93 +1,53 @@
 <template>
   <v-container grid-list-lg>
-    <v-layout row fill-height wrap>
-      <v-flex
-        lg4
-        md6
-        sm8
-        xs12
-        offset-sm2
-        offset-md0
-        v-for="(report, index) in reports"
-        :key="index"
-      >
-        <baseReportComponent :report="report"></baseReportComponent>
+    <v-layout row fill-height justify-center wrap>
+      <v-flex xs10 v-for="(report, index) in reports" :key="index">
+        <BaseReport :tables="report.tables"></BaseReport>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import BaseReportComponent from "@/components/base/BaseReportComponent.vue";
+import BaseReport from "@/components/base/BaseReportComponent.vue";
 
 export default {
   name: "reports",
   components: {
-    BaseReportComponent
+    BaseReport
   },
   data: function() {
     return {
       reports: [
         {
-          username: "User",
-          surname: "McUserFace",
-          aka: "Alter",
-          currentStandardTarget: 2,
-          dietaryEstimatedHydration: 0.98,
-          currentTotalAdjustments: 0.25 + 0.37,
-          hydrationAdjustment: 2 - (0.98 + 0.62),
-          conditions: [
+          tables: [
             {
-              description: "Lorum, descriptum",
-              adjustment: 0.25
-            },
-            {
-              description: "There was drinking",
-              adjustment: 0.37
-            }
-          ],
-          dayReports: [
-            {
-              selectedDate: new Date("18 Dec 2019 00:00:00 GMT"),
-              hydrationTarget: 2,
-              hydrationActual: 2.3,
-              percentageHydration: 115,
-              carerComments: [
-                "Hydration has occurred.",
-                "This person can fit so much water",
-                "I can't believe they drank"
-              ]
-            }
-          ]
-        },
-        {
-          username: "Dick",
-          surname: "high Docterate",
-          aka: "Hi drate",
-          currentStandardTarget: 2.5,
-          dietaryEstimatedHydration: 1.26,
-          currentTotalAdjustments: 0.35 + 0.49,
-          hydrationAdjustment: 2.5 - (1.26 + 0.84),
-          conditions: [
-            {
-              description: "Quidquid latine dictum sit, altum videtur.",
-              adjustment: 0.35
-            },
-            {
-              description: "*glug* *glug*",
-              adjustment: 0.49
-            }
-          ],
-          dayReports: [
-            {
-              selectedDate: new Date("18 Dec 2019 00:00:00 GMT"),
-              hydrationTarget: 2,
-              hydrationActual: 2.3,
-              percentageHydration: 115,
-              carerComments: [
-                "They downloaded moisture successfully.",
-                "They did a glug",
-                "They thought they could drink without me knowing, foolish."
+              type: "header",
+              items: [
+                {
+                  title: "Name",
+                  data: "Test Datson"
+                },
+                {
+                  title: "AKA",
+                  data: "Notrelnam"
+                },
+                {
+                  title: "Current Standard Target",
+                  data: "2.5"
+                },
+                {
+                  title: "Dietary estimated hydration",
+                  data: "1.26"
+                },
+                {
+                  title: "Current Total Adjustments",
+                  data: "0.84"
+                },
+                {
+                  title: "Hydration Adjustment",
+                  data: "0.4"
+                }
               ]
             }
           ]
