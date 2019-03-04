b323fa30691514caaeb073ea51c8410b7683e67d
Anne van der Meer <anne@ndevr.co.uk>
2019-03-04 15:58:58 +0000

    Finished the calender components. Work towards ED-51

--- src/components/base/BaseCalendarComponent.vue
+++ src/components/base/BaseCalendarComponent.vue
@@ -6,9 +6,13 @@
       <v-sheet height="650">
         <v-calendar :value="today" color="primary">
           <template v-slot:day="{ date }">
-            <calendarEvent v-for="(event, index) in eventsMap[date]" :key="index" :title="event.category" :color="'green'" :menuText="event.hydrationLevel">
-              
-            </calendarEvent>
+            <calendarEvent
+              v-for="(event, index) in eventsMap[date]"
+              :key="index"
+              :title="event.category"
+              :color="getColorByStatus(event.category)"
+              :menuText="event.hydrationLevel"
+            ></calendarEvent>
           </template>
         </v-calendar>
       </v-sheet>
@@ -17,59 +21,33 @@
 </template>
 
 <script>
-// import apiLib from "C:/Users/tom/Documents/Dev/eDroplet/ui/src/services/apiLib.js";
 import calendarEvent from '@/components/base/BaseCalendarEventComponent'
 
 export default {
-  components: {calendarEvent},
+  components: { calendarEvent },
   data: () => ({
-    title: "Consumption per day",
-    today: Date(),
-    events: [
-      {
-        category: 'Hydrated',
-        hydrationLevel: '9L/6L',
-        date: '2019-03-01',
-      }
-    ],
-    // colors: custom function/class determines what color will be used depending on the category,
-    category: ["Hydrated", "Little Dehydrated", "Dehydrated"],
-    // category: apiLib.getData('/carer/condition/:userId', description, auth),
-    hydrationLevel: ["5.0/6.0L", "3.0/6.0L", "1.0/6.0L"]
-    // hydrationLevel: apiLib.getData('/carer/reports-snapshot/:userId', aggregatedHydration, hydrationTarget, auth),
+    today: Date()
   }),
   props: {
-    dayReports: Array,
+    events: Array,
     colorStatusPairs: Array
   },
   computed: {
-    eventsMap () {
-      const map = {};
-      this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e));
-      return map;
+    eventsMap: function () {
+      const map = {}
+      this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e))
+      return map
     }
   },
   methods: {
-    getColorStatusPair: function(status) {
-      for (let i = 0; i < this.category.length; i++)
-        for (let j = 0; j < this.colorStatusPairs.length; j++)
-          if (this.category[i] === this.colorStatusPairs[j])
-            return this.colorStatusPairs[j];
+    getColorByStatus: function (status) {
+      for (let i = 0; i < this.colorStatusPairs.length; i++) {
+        if (this.colorStatusPairs[i].status.toLowerCase() === status.toLowerCase()) {
+          return this.colorStatusPairs[i].color
+        }
+      }
+      return 'primary'
     }
   }
-};
+}
 </script>
-
- <style lang="scss" scoped>
-@import "./public/scss/main.scss";
-
-.calendarTitle {
-  margin-left: 7.5px;
-  margin-top: 5px;
-}
-.drinkAmount {
-  text-align: end;
-  margin-top: 38px;
-  margin-right: 7.5px;
-}
-</style>
