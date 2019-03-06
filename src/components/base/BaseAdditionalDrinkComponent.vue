9d19ca945a17fb91a135995d3178f1b5621f4cd8
tom <tom@ndevr.uk>
2019-03-06 17:35:54 +0000

    Created base shell for Additional Drinks View. Work towards ED-34

---
+++ src/components/base/BaseAdditionalDrinkComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-layout>
+        <v-flex>
+          <v-card-title>
+            <h1>{{ title }}</h1>
+          </v-card-title>
+        </v-flex>
+        <v-spacer></v-spacer>
+        <v-flex xs1 mt-4>
+          <v-dialog width="500">
+            <template v-slot:activator="{ on }">
+              <v-btn class="primary" v-on="on">Add Drink</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <h3>Drink's Name</h3>
+              </v-card-title>
+              <v-card-text>
+                <v-input label="Name" v-model="name"></v-input>
+              </v-card-text>
+              <v-card-title>
+                <h3>Amount Drank</h3>
+              </v-card-title>
+              <v-card-text>
+                <v-input label="Amount" v-model="amount"></v-input>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </v-flex>
+
+        <v-flex xs4>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+        </v-flex>
+      </v-layout>
+
+      <v-data-table :headers="headers" :items="drinks" :search="search" must-sort>
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.amount }}</td>
+        </template>
+        <v-alert
+          v-slot:no-results
+          :value="true"
+          color="error"
+          icon="warning"
+        >Your search for "{{ search }}" found no results.</v-alert>
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import StoreAditionalDrinks from "../../store/StoreAditionalDrinks.js";
+
+export default {
+  mixins: {
+    StoreAditionalDrinks
+  },
+  data() {
+    return {
+      title: "Add Additional Drinks",
+      search: "",
+      headers: [
+        { text: "Name", value: "name" },
+        { text: "Amount", sortable: false, value: "amount" }
+      ],
+      drinks: []
+    };
+  },
+  methods: {
+    addDrink: function() {
+      //
+    }
+  }
+};
+</script>
---
+++ src/views/carer/AdditionalDrinks.vue
@@ -0,0 +1,13 @@
+<template>
+  <additional-drinks></additional-drinks>  
+</template>
+
+<script>
+import BaseAdditionalDrinkComponent from '../../components/base/BaseAdditionalDrinkComponent.vue';
+
+export default {
+  components: {
+    'additional-drinks': BaseAdditionalDrinkComponent
+  },
+}
+</script>
