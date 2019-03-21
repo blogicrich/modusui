d4bc609c5164a85e14e3cedded1228ada52e638b
Anne van der Meer <anne@ndevr.co.uk>
2019-03-21 10:28:35 +0000

    Work towards ED-110

--- src/views/carer/Alerts.vue
+++ src/views/carer/Alerts.vue
@@ -1,6 +1,10 @@
 <template>
   <v-container>
-    <h2 v-if="this.$vuetify.breakpoint.mdAndDown" class="pg-subheader text-primary text-center mx-3" text-xs-center>System Administrators</h2>
+    <h2
+      v-if="this.$vuetify.breakpoint.mdAndDown"
+      class="pg-subheader text-primary text-center mx-3"
+      text-xs-center
+    >System Administrators</h2>
     <BaseDataTable
       :headers="headers"
       :items="items"
@@ -32,10 +36,9 @@
 </template>
 
 <script>
-
 import { crudRoutines } from '@/mixins/dataTableCRUD.js'
 import BaseDataTable from '@/components/base/BaseDataTableComponent.vue'
-import { log } from 'util';
+import { log } from 'util'
 
 export default {
   name: 'SystemAdmins',
@@ -54,51 +57,206 @@ export default {
       errorMsg: ' ',
       loadingMsg: ' ',
       loadedMsg: ' ',
-      readUrl: 'carer/alerts/23/1552470795',
+      readUrl:
+        'carer/alerts/' +
+        this.$store.state.userId +
+        '/' +
+        this.$store.state.date,
       primaryColor: 'primary',
       secondaryColor: 'primary darken-2',
       icon: 'warning',
       iconAdd: 'person_add',
       headers: [
-        { text: 'PortalId', value: 'portalAuthorisedId', align: 'left', sortable: false, cellType: 'tb', hidden: true, editable: false },
-        { text: 'Title', value: 'titleId', align: 'left', sortable: false, cellType: 'md', hidden: true, editable: true },
-        { text: 'Description', value: 'shortDescription', align: 'left', sortable: false, cellType: 'tb', hidden: false, editable: true },
-        { text: 'Given Name', value: 'givenName', align: 'left', sortable: false, cellType: 'tb', hidden: false, editable: true },
-        { text: 'Family Name', value: 'familyName', align: 'left', sortable: false, cellType: 'tb', hidden: false, editable: true },
-        { text: 'Salutation', value: 'salutation', align: 'left', sortable: false, cellType: 'tb', hidden: false, editable: true },
-        { text: 'Username', value: 'username', align: 'left', sortable: false, cellType: 'tb', hidden: true, editable: true },
-        { text: 'Corporate ID', value: 'corporateIdentification', cellType: 'tb', hidden: true, editable: true },
+        {
+          text: 'PortalId',
+          value: 'portalAuthorisedId',
+          align: 'left',
+          sortable: false,
+          cellType: 'tb',
+          hidden: true,
+          editable: false
+        },
+        {
+          text: 'Title',
+          value: 'titleId',
+          align: 'left',
+          sortable: false,
+          cellType: 'md',
+          hidden: true,
+          editable: true
+        },
+        {
+          text: 'datetime',
+          value: 'dateTime',
+          align: 'left',
+          sortable: true,
+          cellType: 'tb',
+          hidden: false,
+          editable: false
+        },
+        {
+          text: 'Alert Type',
+          value: 'description',
+          align: 'left',
+          sortable: false,
+          cellType: 'tb',
+          hidden: false,
+          editable: false
+        },
+        {
+          text: 'Corporate ID',
+          value: 'corporateIdentification',
+          cellType: 'tb',
+          hidden: true,
+          editable: true
+        }
       ],
       newItem: [
-        { titleId: 0, cellType: 'md', attr: 'titleId', cellLabel: 'Title', displayVal: 'shortDescription', returnVal: 'titleId', menuItems: [], validators: [] },
-        { givenName: ' ', cellType: 'tb', attr: 'givenName', cellLabel: 'Given Name', menuItems: [], validators: [] },
-        { familyName: ' ', cellType: 'tb', attr: 'familyName', cellLabel: 'Family Name', menuItems: [], validators: [] },
-        { corporateIdentification: 0, cellType: 'tb', attr: 'corporateIdentification', cellLabel: 'Company', menuItems: [], validators: [] },
-        { salutation: ' ', cellType: 'tb', attr: 'salutation', cellLabel: 'Salutation', menuItems: [], validators: [] },
-        { username: ' ', cellType: 'tb', attr: 'username', cellLabel: 'Username', menuItems: [], validators: [] },
-        { password: ' ', cellType: 'tb', attr: 'password', cellLabel: 'Password', menuItems: [], validators: [] },
+        {
+          titleId: 0,
+          cellType: 'md',
+          attr: 'titleId',
+          cellLabel: 'Title',
+          displayVal: 'shortDescription',
+          returnVal: 'titleId',
+          menuItems: [],
+          validators: []
+        },
+        {
+          givenName: ' ',
+          cellType: 'tb',
+          attr: 'givenName',
+          cellLabel: 'Given Name',
+          menuItems: [],
+          validators: []
+        },
+        {
+          familyName: ' ',
+          cellType: 'tb',
+          attr: 'familyName',
+          cellLabel: 'Family Name',
+          menuItems: [],
+          validators: []
+        },
+        {
+          corporateIdentification: 0,
+          cellType: 'tb',
+          attr: 'corporateIdentification',
+          cellLabel: 'Company',
+          menuItems: [],
+          validators: []
+        },
+        {
+          salutation: ' ',
+          cellType: 'tb',
+          attr: 'salutation',
+          cellLabel: 'Salutation',
+          menuItems: [],
+          validators: []
+        },
+        {
+          username: ' ',
+          cellType: 'tb',
+          attr: 'username',
+          cellLabel: 'Username',
+          menuItems: [],
+          validators: []
+        },
+        {
+          password: ' ',
+          cellType: 'tb',
+          attr: 'password',
+          cellLabel: 'Password',
+          menuItems: [],
+          validators: []
+        }
       ],
       defaultItem: [
-        { titleId: 0, portalAuthorisedId: 0, givenName: '', familyName: '', corporateIdentification: '', username: '', password: '' }
+        {
+          titleId: 0,
+          portalAuthorisedId: 0,
+          givenName: '',
+          familyName: '',
+          corporateIdentification: '',
+          username: '',
+          password: ''
+        }
       ],
-      urls: [
-        { url: 'sysadmin/title', attr: 'titleId', key: 'titleId' },
-      ]
+      urls: [{ url: 'sysadmin/title', attr: 'titleId', key: 'titleId' }]
     }
   },
   methods: {
     resetItem () {
       this.newItem = [
-        { titleId: 0, cellType: 'md', attr: 'titleId', cellLabel: 'Title', displayVal: 'shortDescription', returnVal: 'titleId', menuItems: [], validators: [] },
-        { givenName: ' ', cellType: 'tb', attr: 'givenName', cellLabel: 'Given Name', menuItems: [], validators: [] },
-        { familyName: ' ', cellType: 'tb', attr: 'familyName', cellLabel: 'Family Name', menuItems: [], validators: [] },
-        { corporateIdentification: 0, cellType: 'tb', attr: 'corporateIdentification', cellLabel: 'Company', menuItems: [], validators: [] },
-        { salutation: ' ', cellType: 'tb', attr: 'salutation', cellLabel: 'Family Name', menuItems: [], validators: [] },
-        { username: ' ', cellType: 'tb', attr: 'username', cellLabel: 'Username', menuItems: [], validators: [] },
-        { password: ' ', cellType: 'tb', attr: 'password', cellLabel: 'Password', menuItems: [], validators: [] },
+        {
+          titleId: 0,
+          cellType: 'md',
+          attr: 'titleId',
+          cellLabel: 'Title',
+          displayVal: 'shortDescription',
+          returnVal: 'titleId',
+          menuItems: [],
+          validators: []
+        },
+        {
+          givenName: ' ',
+          cellType: 'tb',
+          attr: 'givenName',
+          cellLabel: 'Given Name',
+          menuItems: [],
+          validators: []
+        },
+        {
+          familyName: ' ',
+          cellType: 'tb',
+          attr: 'familyName',
+          cellLabel: 'Family Name',
+          menuItems: [],
+          validators: []
+        },
+        {
+          corporateIdentification: 0,
+          cellType: 'tb',
+          attr: 'corporateIdentification',
+          cellLabel: 'Company',
+          menuItems: [],
+          validators: []
+        },
+        {
+          salutation: ' ',
+          cellType: 'tb',
+          attr: 'salutation',
+          cellLabel: 'Family Name',
+          menuItems: [],
+          validators: []
+        },
+        {
+          username: ' ',
+          cellType: 'tb',
+          attr: 'username',
+          cellLabel: 'Username',
+          menuItems: [],
+          validators: []
+        },
+        {
+          password: ' ',
+          cellType: 'tb',
+          attr: 'password',
+          cellLabel: 'Password',
+          menuItems: [],
+          validators: []
+        }
       ]
       this.defaultItem = [
-        { titleId: 0, portalAuthorisedId: 0, givenName: '', familyName: '', corporateIdentification: '', username: '', password: '' }
+        {
+          titleId: 0,
+          portalAuthorisedId: 0,
+          givenName: '',
+          familyName: '',
+          corporateIdentification: '',
+          username: '',
+          password: ''
+        }
       ]
     },
     async setData () {
@@ -113,7 +271,7 @@ export default {
           }
         }
       }
-    },
+    }
   },
   mounted () {
     this.getItems(this.readUrl)
@@ -121,5 +279,5 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-  @import "./public/scss/main.scss";
+@import "./public/scss/main.scss";
 </style>
