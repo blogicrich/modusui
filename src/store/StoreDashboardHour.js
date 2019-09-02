c599f8cadddacbf8ced865b993f5b7b9ba3a6b39
Sjoerd Dal <sjoerddal@gmail.com>
2019-09-02 17:37:08 +0200

    Fixed if condition

--- src/store/StoreDashboardHour.js
+++ src/store/StoreDashboardHour.js
@@ -15,10 +15,10 @@ export const moduleDashboardHour = {
     async fetchDashboardHourGet (context) {
       const response = await apiLib.getData('carer/dashboard-hour/' + this.getters.getterUserId + '/' + this.getters.getterDate, false, true)
 
-      if (Array.isArray(typeof response === 'object')) {
-        context.commit('SET_DASHBOARDHOUR', null)
-      } else {
+      if (typeof response === 'object') {
         context.commit('SET_DASHBOARDHOUR', response)
+      } else {
+        context.commit('SET_DASHBOARDHOUR', null)
       }
     }
   }
