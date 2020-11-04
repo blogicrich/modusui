46f5aed914ce6d8ce054e7e0c15ab9d0bab83c32
Sjoerd <sjoerddal108@gmail.com>
2020-11-04 16:20:20 +0100

    Fixed the standard linter but disabled newer but strict rules from Vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,19 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    // '@vue/essential'
+    'standard',
+    'plugin:vue/recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
+    'vue/attribute-hyphenation': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/order-in-components': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/attributes-order': 'off',
+    'vue/max-attributes-per-line': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
