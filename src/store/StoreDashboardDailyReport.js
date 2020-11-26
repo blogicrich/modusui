75f973e36a102baf2478d71d346fca7e8a911d74
blogicdevs <richard.reading@bloclogic.co.uk>
2020-11-26 00:31:44 +0000

    - Add try catch to store functions
    - Partially Resolves: ED-214

--- src/store/StoreDashboardDailyReport.js
+++ src/store/StoreDashboardDailyReport.js
@@ -1,6 +1,8 @@
 import apiLib from '../services/apiLib.js'
 import moment from 'moment'
 
+const bounce = 500
+
 export const moduleDashboardDailyReport = {
   state: {
     comments: [],
@@ -11,7 +13,12 @@ export const moduleDashboardDailyReport = {
     newCommentDate: getToday(),
     maxDate: getToday(),
     // Comment for edit or deletion
-    selectedComment: {}
+    selectedComment: {},
+    // Store Booleans
+    commentsLoading: false,
+    commentsError: false,
+    commentsUpdating: false,
+    commentsDeleting: false
   },
   mutations: {
     // CREATE
@@ -28,7 +35,6 @@ export const moduleDashboardDailyReport = {
       state.newComment = data
     },
     UPDATE_NEW_COMMENT_DATE (state, data) {
-      console.log(data)
       state.newCommentDate = data
     },
     RESET_NEW_COMMENT_STATE (state) {
@@ -55,87 +61,126 @@ export const moduleDashboardDailyReport = {
       state.comments = data
     },
     SET_DAILY_REPORT_LOAD_STATE (state, data) {
-      state.drinksLoading = data
+      state.commentsLoading = data
+    },
+    SET_DAILY_REPORT_ERROR_STATE (state, data) {
+      state.commentsError = data
+    },
+    SET_DAILY_REPORT_UPDATE_STATE (state, data) {
+      state.commentsUpdating = data
+    },
+    SET_DAILY_REPORT_DELETE_STATE (state, data) {
+      state.commentsDeleting = data
     },
     SET_SELECTED_COMMENT (state, data) {
-      state.selectedComment = { ...data }
+      state.selectedComment = { ...data.item }
     },
     RESET_DAILY_REPORT_STATE (state) {
       state.comments = []
       state.newComments = []
       state.selectedComment = {}
-      state.dailyReportsLoading = false
-      state.dailyReportError = false
       state.newComment = ''
       state.newCommentDateTime = null
+      state.commentsLoading = false
+      state.commentsError = false
+      state.commentsUpdating = false
+      state.commentsDeleting = false
     }
   },
   actions: {
     async fetchDailyReport (context) {
-      const date = context.rootState.dashboardDates.dashboardUnixDate
-
-      context.commit('SET_DAILY_REPORT_LOAD_STATE', true)
-      apiLib.getData('carer/day-report/' + date).then((response) => {
+      try {
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', false)
+        context.commit('SET_DAILY_REPORT_LOAD_STATE', true)
+        const date = context.rootState.dashboardDates.dashboardUnixDate
+        const response = await apiLib.getData('carer/day-report/' + date)
         const dailyReport = []
-        for (let i = 0; i < response.length; i++) {
-          let element = { ...response[i] }
-          const date = convertTimeToLongForm(element.date * 1000)
-          element = { ...element, date: date }
-          dailyReport.push(element)
-        }
-        if (typeof response === 'undefined') {
+        if (typeof response === 'undefined' || !Array.isArray(response)) {
           context.commit('SET_COMMENTS', [])
           context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
+          context.commit('SET_DAILY_REPORT_ERROR_STATE', true)
         } else {
-          context.commit('SET_COMMENTS', dailyReport)
-          context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
+          for (let i = 0; i < response.length; i++) {
+            let element = { ...response[i] }
+            const date = convertTimeToLongForm(element.date * 1000)
+            element = { ...element, date: date }
+            dailyReport.push(element)
+          }
+          setTimeout(() => {
+            context.commit('SET_COMMENTS', dailyReport)
+            context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
+          }, bounce)
         }
-      })
+      } catch (error) {
+        console.error(error)
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', true)
+        context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
+      }
     },
     async postNewComments (context) {
-      const userId = context.rootState.dashboardUsers.selectedUser.userId
-      const jobs = []
-
-      context.commit('SET_DAILY_REPORT_LOAD_STATE', true)
-      for (let i = 0; i < context.state.newComments.length; i++) {
-        const element = context.state.newComments[i]
-        const data = {
-          userId: userId,
-          comments: element.comment,
-          date: convertToUnix(element.date) / 1000
+      try {
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', false)
+        context.commit('SET_DAILY_REPORT_UPDATE_STATE', true)
+        const userId = context.rootState.dashboardUsers.selectedUser.userId
+        const jobs = []
+        for (let i = 0; i < context.state.newComments.length; i++) {
+          const element = context.state.newComments[i]
+          const data = {
+            userId: userId,
+            comments: element.comment,
+            date: convertToUnix(element.date) / 1000
+          }
+          const update = apiLib.postData('carer/day-report/', data, false, true)
+          jobs.push(update)
         }
-        const update = apiLib.postData('carer/day-report/', data, false, true)
-        jobs.push(update)
-      }
-      await Promise.all(jobs).then((response) => {
+        await Promise.all(jobs)
         context.dispatch('fetchDailyReport')
-      }).finally(
-        context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
-      )
+        setTimeout(() => {
+          context.commit('SET_DAILY_REPORT_UPDATE_STATE', false)
+        }, bounce)
+      } catch (error) {
+        console.error(error)
+        context.commit('SET_DAILY_REPORT_UPDATE_STATE', false)
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', true)
+      }
     },
     async updateComment (context) {
-      const dayReportId = context.state.selectedComment.dayReportId
-      const userId = context.rootState.dashboardUsers.selectedUser.userId
-      const payload = {
-        userId: userId,
-        comments: context.state.selectedComment.comments,
-        date: convertToUnix(context.state.selectedComment.date) / 1000
-      }
-      context.commit('SET_DAILY_REPORT_LOAD_STATE', true)
-      await apiLib.updateData('carer/day-report/' + dayReportId, payload, false, true).then(
+      try {
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', false)
+        context.commit('SET_DAILY_REPORT_UPDATE_STATE', true)
+        const dayReportId = context.state.selectedComment.dayReportId
+        const userId = context.rootState.dashboardUsers.selectedUser.userId
+        const payload = {
+          userId: userId,
+          comments: context.state.selectedComment.comments,
+          date: convertToUnix(context.state.selectedComment.date) / 1000
+        }
+        await apiLib.updateData('carer/day-report/' + dayReportId, payload, false, true)
+        setTimeout(() => {
+          context.commit('SET_DAILY_REPORT_UPDATE_STATE', false)
+        }, bounce)
         context.dispatch('fetchDailyReport')
-      ).finally(
-        context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
-      )
-      // return update
+      } catch (error) {
+        console.error(error)
+        context.commit('SET_DAILY_REPORT_UPDATE_STATE', false)
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', true)
+      }
     },
     async deleteComment (context) {
-      const dayReportId = context.state.selectedComment.dayReportId
-      context.commit('SET_DAILY_REPORT_LOAD_STATE', true)
-      const update = await apiLib.deleteData('carer/day-report/' + dayReportId, false, true).then(
-        context.commit('SET_DAILY_REPORT_LOAD_STATE', false)
-      )
-      return update
+      try {
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', false)
+        context.commit('SET_DAILY_REPORT_DELETE_STATE', true)
+        const dayReportId = context.state.selectedComment.dayReportId
+        await apiLib.deleteData('carer/day-report/' + dayReportId, false, true)
+        context.dispatch('fetchDailyReport')
+        setTimeout(() => {
+          context.commit('SET_DAILY_REPORT_UPDATE_STATE', false)
+        }, bounce)
+      } catch (error) {
+        console.error(error)
+        context.commit('SET_DAILY_REPORT_DELETE_STATE', false)
+        context.commit('SET_DAILY_REPORT_ERROR_STATE', true)
+      }
     }
   }
 }
